f6efe9c6cbd48861ef6c3cea96daee9040dd5cad
Sriram Jayashankar <sriramjayashankar@gmail.com>
2024-02-11 18:18:55 +0530

    stable working till 2.49.02 modified post

--- app/src/main/java/com/example/instaclone/Utils/Contsant.kt
+++ app/src/main/java/com/example/instaclone/Utils/Contsant.kt
@@ -2,3 +2,4 @@ package com.example.instaclone.Utils
 
 const val USER_NODE="User"
 const val USER_PROFILE_FOLDER="Profile"
+const val POST_FOLDER="PostImages"
--- app/src/main/java/com/example/instaclone/posts/PostActivity.kt
+++ app/src/main/java/com/example/instaclone/posts/PostActivity.kt
@@ -2,12 +2,25 @@ package com.example.instaclone.posts
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContracts
 import com.example.instaclone.R
+import com.example.instaclone.Utils.POST_FOLDER
+import com.example.instaclone.Utils.USER_PROFILE_FOLDER
+import com.example.instaclone.Utils.uploadImage
 import com.example.instaclone.databinding.ActivityPostBinding
 
 class PostActivity : AppCompatActivity() {
     val binding by lazy {
         ActivityPostBinding.inflate(layoutInflater)
+    }
+    private val launcher = registerForActivityResult(ActivityResultContracts.GetContent()) { uri ->
+        uri?.let {
+            uploadImage(uri, POST_FOLDER) {
+                if (it!= null) {
+    binding.imageView2.setImageURI(uri)
+                }
+            }
+        }
     }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,7 +31,9 @@ class PostActivity : AppCompatActivity() {
         binding.materialtoolbar.setOnClickListener{
             finish()
         }
-
+        binding.imageView2.setOnClickListener{
+            launcher.launch("image/*")
+        }
 
     }
 }
